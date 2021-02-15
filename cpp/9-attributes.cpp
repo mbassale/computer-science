5b178f18e418142e3fa7db03ad4d1ea36f51c232
Marco Bassaletti <bassaletti@gmail.com>
2021-02-14 22:55:37 -0300

    C++ Crash Course: Chapter 8: Expressions.

---
+++ cpp/9-attributes.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <stdexcept>
+
+[[noreturn]] void pitcher() {
+    throw std::runtime_error{ "Knuckleball." };
+}
+
+int main() {
+    try {
+        pitcher();
+    } catch(const std::exception& e) {
+        printf("exception: %s\n", e.what());
+    }
+    return 0;
+}
---
+++ cpp/9-structured-bindings.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+
+struct TextFile {
+    bool success;
+    const char* data;
+    size_t n_bytes;
+};
+
+TextFile read_text_file(const char* path) {
+    const static char contents[] { "Sometimes the goat is you." };
+    return TextFile {
+        true,
+        contents,
+        sizeof(contents)
+    };
+}
+
+int main() {
+    if (const auto [success, contents, length] = read_text_file("Readme.txt"); success) {
+        printf("Read %zd bytes: %s\n", length, contents);
+    } else {
+        printf("Failed to open Readme.txt");
+    }
+    return 0;
+}
