d7f25c80e8030ecec73707c473d1f7da0102738e
Marco Bassaletti <bassaletti@gmail.com>
2021-03-11 17:51:08 -0300

    Quick sort with TDD O(n*Log(n)).

--- java/src/cl/mbassale/cleanCoders/sort/SortTest.java
+++ java/src/cl/mbassale/cleanCoders/sort/SortTest.java
@@ -25,35 +25,44 @@ public class SortTest {
         assertSorted(intList(), intList());
         assertSorted(intList(1), intList(1));
         assertSorted(intList(2, 1), intList(1, 2));
+        assertSorted(intList(2, 1, 3), intList(1, 2, 3));
+        assertSorted(intList(2, 3, 1), intList(1, 2, 3));
         assertSorted(intList(1, 3, 2), intList(1, 2, 3));
         assertSorted(intList(3, 2, 1), intList(1, 2, 3));
-        sortBigList(25000);
+        assertSorted(intList(3, 2, 2, 1), intList(1, 2, 2, 3));
+        sortBigList(50000);
     }
 
     private void sortBigList(int n) {
         List<Integer> unsorted = new ArrayList<Integer>();
         for (int i = 0; i < n; i++)
-            unsorted.add((int)(Math.random() * 1000.0));
+            unsorted.add((int) (Math.random() * 1000.0));
         List<Integer> sorted = sort(unsorted);
         for (int i = 0; i < n - 1; i++)
             assertTrue(sorted.get(i) <= sorted.get(i + 1));
     }
 
     private List<Integer> sort(List<Integer> list) {
-        for (int size = list.size(); size >= 0; size--)
-            for (int index = 0; size > index + 1; index++)
-                if (outOfOrder(list, index))
-                    swap(list, index);
-        return list;
+        List<Integer> sorted = new ArrayList<>();
+        if (list.size() <= 1)
+            return list;
+        else if (list.size() == 2) {
+            return Arrays.asList(Math.min(list.get(0), list.get(1)), Math.max(list.get(0), list.get(1)));
+        } else {
+            List<Integer> leftElements = new ArrayList<>();
+            int middle = list.get(0);
+            List<Integer> rightElements = new ArrayList<>();
+            for (int i : list.subList(1, list.size())) {
+                if (i > middle)
+                    rightElements.add(i);
+                else
+                    leftElements.add(i);
+            }
+            sorted.addAll(sort(leftElements));
+            sorted.add(middle);
+            sorted.addAll(sort(rightElements));
+        }
+        return sorted;
     }
 
-    private boolean outOfOrder(List<Integer> list, int index) {
-        return list.get(index) > list.get(index + 1);
-    }
-
-    private void swap(List<Integer> list, int index) {
-        var temp = list.get(index);
-        list.set(index, list.get(index + 1));
-        list.set(index + 1, temp);
-    }
 }
