0e92ff358e3915bfac98e73b4f5fe7565273aebd
Marco Bassaletti <bassaletti@gmail.com>
2021-04-08 21:29:09 -0400

    Book Head First Design Patterns. 2nd Edition. Chapter 10. State Pattern. Naive implementation.

---
+++ book5-head-first-design-patterns/test/hfdp/ch10/GumballMachineTest.java
@@ -0,0 +1,44 @@
+package hfdp.ch10;
+
+import cl.mbassale.hfdp.ch10.GumballMachine;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GumballMachineTest {
+
+    @Test
+    void gumballMachine() {
+        var gumballMachine = new GumballMachine(5);
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.ejectQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.ejectQuarter();
+
+        System.out.println(gumballMachine);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        System.out.println(gumballMachine);
+    }
+}
