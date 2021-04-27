2f4c905eb195a3a099f4be622cf879b3b0e642b8
Marco Bassaletti <bassaletti@gmail.com>
2021-04-27 00:18:55 -0400

    Greatest common divisor, recursive algorithm.

--- book2-algorithms-sedgewick/ch1/08-euclid-algorithm.py
+++ book2-algorithms-sedgewick/ch1/08-euclid-algorithm.py
@@ -3,16 +3,23 @@ import unittest
 
 def gcd(p, q):
     if q == 0:
-        return pcd
+        return p
     r = p % q
     return gcd(q, r)
 
 
 class GCDTest(unittest.TestCase):
 
-    def test_zero():
+    def test_zero(self):
         self.assertEqual(gcd(0, 0), 0)
         self.assertEqual(gcd(10, 0), 10)
+        self.assertEqual(gcd(-10, 0), -10)
+
+    def test_gcd(self):
+        self.assertEqual(gcd(6, 3), 3)
+        self.assertEqual(gcd(64, 8), 8)
+        self.assertEqual(gcd(81, 7), 1)
+        self.assertEqual(gcd(-81, 7), 1)
 
 
 if __name__ == '__main__':
