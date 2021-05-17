3ced7236c693b812c94e06052c685d14664c156f
Marco Bassaletti <bassaletti@gmail.com>
2021-05-17 18:30:52 -0400

    Two sum algorithm: Binary search implementation using STL.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

---
+++ book2-algorithms-sedgewick/ch1/29_two_sum_fast.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <chrono>
+
+size_t two_sum_fast(std::vector<long> &numbers) {
+    size_t sum_count = 0;
+    // n * log(n) sorting.
+    std::sort(numbers.begin(), numbers.end());
+    auto it = numbers.begin();
+    while (it != numbers.end()) {
+        auto num = *it;
+        // perform binary search from this number onwards to prevent double counting.
+        if (std::binary_search(it, numbers.end(), -num)) {
+            sum_count++;
+        }
+        it++;
+    }
+    return sum_count;
+}
+
+int main(int argc, char *argv[]) {
+    using namespace std;
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::milliseconds;
+
+    cout << "Two Sum Problem with Binary Search." << endl;
+
+    if (argc < 2) {
+        cerr << "Missing data filename." << endl;
+        return EXIT_FAILURE;
+    }
+
+    cout << "Loading data." << endl;
+
+    stringstream data_filename;
+    data_filename << "./data/" << argv[1];
+    ifstream in_file(data_filename.str(), ios::in);
+    if (!in_file.is_open()) {
+        cerr << "Cannot open data filename: " << data_filename.str() << endl;
+        return EXIT_FAILURE;
+    }
+
+    vector<long> numbers;
+    string line;
+    while (getline(in_file, line)) {
+        auto number = stol(line);
+        numbers.push_back(number);
+    }
+
+    cout << "Read " << numbers.size() << " integers." << endl;
+
+    cout << "Calculating... " << flush;
+    auto t1 = high_resolution_clock::now();
+    auto sum_count = two_sum_fast(numbers);
+    auto t2 = high_resolution_clock::now();
+    cout << "Done." << endl;
+    auto running_time = duration_cast<milliseconds>(t2 - t1).count();
+    cout << "Number of Zero Two Sum: " << sum_count << " Running Time: " << running_time << "ms" << endl;
+    return EXIT_SUCCESS;
+}
