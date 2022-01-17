1e6bbe6cf32c27cd3210525c03374176c707ee8d
Marco Bassaletti <bassaletti@gmail.com>
2022-01-16 17:07:40 -0800

    Go: function variables

---
+++ golang/15-functions2.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type opFuncType func(int, int) int
+
+func add(i int, j int) int { return i + j }
+
+func sub(i int, j int) int { return i - j }
+
+func mul(i int, j int) int { return i * j }
+
+func div(i int, j int) int { return i / j }
+
+func mod(i int, j int) int { return i % j }
+
+var opMap = map[string]opFuncType{
+	"+": add,
+	"-": sub,
+	"*": mul,
+	"/": div,
+	"%": mod,
+}
+
+func main() {
+	expressions := [][]string{
+		{"2", "+", "3"},
+		{"2", "-", "3"},
+		{"2", "*", "3"},
+		{"2", "/", "3"},
+		{"2", "%", "3"},
+		{"two", "+", "three"},
+		{"5"},
+	}
+
+	for _, expression := range expressions {
+		if len(expression) != 3 {
+			fmt.Println("Invalid expression:", expression)
+			continue
+		}
+		p1, err := strconv.Atoi(expression[0])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		op := expression[1]
+		opFunc, ok := opMap[op]
+		if !ok {
+			fmt.Println("Unsupported operator:", op)
+			continue
+		}
+		p2, err := strconv.Atoi(expression[2])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		result := opFunc(p1, p2)
+		fmt.Printf("%v %v %v = %v\n", p1, op, p2, result)
+	}
+}
