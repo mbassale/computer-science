9b66afbf366713ca580b65ffa5870de99e28e8fb
Marco Bassaletti <bassaletti@gmail.com>
2022-11-28 21:20:44 -0800

    [Book13] Chapter 1: Ackermann Function

---
+++ book13-structure-interpretation-computer-programs/ch1/10-exercise1.10.scm
@@ -0,0 +1,46 @@
+; Exercise 1.10
+
+; Ackermann's Function
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1) 
+                 (A x (- y 1))))))
+
+(A 1 10)
+(A 2 4)
+(A 3 3)
+(A 4 2)
+
+(define (f n) (A 0 n))
+(f 0)
+(f 1)
+(f 2)
+(f 3)
+(f 4)
+(f 5)
+
+(define (g n) (A 1 n))
+(g 0)
+(g 1)
+(g 2)
+(g 3)
+(g 4)
+(g 5)
+
+(define (h n) (A 2 n))
+(h 0)
+(h 1)
+(h 2)
+(h 3)
+(h 4)
+(h 5)
+
+(define (k n) (* 5 n n))
+(k 0)
+(k 1)
+(k 2)
+(k 3)
+(k 4)
+(k 5)
