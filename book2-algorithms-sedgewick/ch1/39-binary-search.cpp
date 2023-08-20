5b94ebb9b1bfe661ffb3ee4ba19ebe1ced5cece4
Marco Bassaletti <bassaletti@gmail.com>
2023-08-19 23:29:02 -0700

    [Sedgewick][Ch1][C++] Binary Search with tinyT.txt & tinyW.txt files and execution output

--- book2-algorithms-sedgewick/ch1/39-binary-search.cpp
+++ book2-algorithms-sedgewick/ch1/39-binary-search.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <vector>
 
@@ -27,10 +29,42 @@ size_t rank(T key, const vector<T>& a, size_t left = 0,
 
 }  // namespace mbassale
 
+/*=============================================================================
+Execution Output:
+mbassale@MacBook-Pro ch1 % ./39-binary-search ../data/tinyW.txt < ../data/tinyT.txt 
+rank(3, {1,2,3}) = 2
+rank(2, {1,2,3}) = 1
+rank(1, {1,2,3}) = 0
+rank(0, {1,2,3}) = 18446744073709551615
+50
+99
+13
+=============================================================================*/
+
 int main(int argc, char* argv[]) {
   cout << "rank(3, {1,2,3}) = " << mbassale::rank(3, {1, 2, 3}) << endl;
   cout << "rank(2, {1,2,3}) = " << mbassale::rank(2, {1, 2, 3}) << endl;
   cout << "rank(1, {1,2,3}) = " << mbassale::rank(1, {1, 2, 3}) << endl;
   cout << "rank(0, {1,2,3}) = " << mbassale::rank(0, {1, 2, 3}) << endl;
+
+  vector<int> numbers;
+  ifstream fin(argv[1]);
+  if (!fin.is_open()) {
+    cerr << "Cannot Open File: " << argv[1];
+  }
+
+  string line;
+  while (!std::getline(fin, line).eof()) {
+    numbers.push_back(stoi(line));
+  }
+  sort(numbers.begin(), numbers.end());
+
+  while (!std::getline(cin, line).eof()) {
+    const auto n = stoi(line);
+    if (mbassale::rank(n, numbers) == SIZE_T_MAX) {
+      cout << n << endl;
+    }
+  }
+
   return EXIT_SUCCESS;
 }
