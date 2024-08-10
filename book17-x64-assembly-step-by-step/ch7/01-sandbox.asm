400670bcce25702381c43960d709f2d555e0d78e
Marco Bassaletti <bassaletti@gmail.com>
2024-08-10 11:40:29 -0700

    [Assembly][Ch7] Sandbox for debugging instructions

---
+++ book17-x64-assembly-step-by-step/ch7/01-sandbox.asm
@@ -0,0 +1,17 @@
+; Chapter 7: Sandbox for testing instructions
+
+section .data
+
+section .bss
+
+section .text
+  global main
+
+main:
+  mov rbp, rsp
+  nop
+
+
+  nop
+
+section .note.GNU-stack noalloc noexec nowrite progbits
