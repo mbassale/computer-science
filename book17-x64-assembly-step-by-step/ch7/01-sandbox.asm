6b653f252f68d9cbfe6de5259469c77160b93349
Marco Bassaletti <bassaletti@gmail.com>
2024-08-10 22:25:16 -0700

    [Assembly][Ch7] Inc, Dec, Jnz

--- book17-x64-assembly-step-by-step/ch7/01-sandbox.asm
+++ book17-x64-assembly-step-by-step/ch7/01-sandbox.asm
@@ -11,14 +11,37 @@ main:
   mov rbp, rsp
   nop
 
+  ; little endian ordering
   mov eax, 'WXYZ'
+  nop
+
+  ; set to zero
   xor rax, rax
+  nop
+
+  ; xchg
+xchg_ex:
+  xor rcx, rcx
   mov cl, 1h
   mov ch, 2h
   xchg cl, ch
-
   nop
 
+  ; inc, dec and flags
+inc_ex:
+  mov ebx, 02Dh
+  dec ebx
+  mov eax, 0FFFFFFFFh
+  inc eax
+
+  ; loop
+loop_ex:
+  mov rax, 5
+loop01:
+  dec rax
+  jnz loop01
+
+  nop
   mov rax, 60   ; exit syscall
   mov rdi, 0
   syscall
