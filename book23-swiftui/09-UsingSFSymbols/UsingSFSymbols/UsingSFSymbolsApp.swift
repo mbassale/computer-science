ff07c10afa8e8777b79a390b66ef323689bf9ec3
Marco Bassaletti <bassaletti@gmail.com>
2025-08-20 12:46:26 -0400

    [Book23-SwiftUI][Ch1] Using SF Symbols

---
+++ book23-swiftui/09-UsingSFSymbols/UsingSFSymbols/UsingSFSymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingSFSymbolsApp.swift
+//  UsingSFSymbols
+//
+//  Created by Marco Bassaletti on 20-08-25.
+//
+
+import SwiftUI
+
+@main
+struct UsingSFSymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ book23-swiftui/09-UsingSFSymbols/UsingSFSymbolsTests/UsingSFSymbolsTests.swift
@@ -0,0 +1,17 @@
+//
+//  UsingSFSymbolsTests.swift
+//  UsingSFSymbolsTests
+//
+//  Created by Marco Bassaletti on 20-08-25.
+//
+
+import Testing
+@testable import UsingSFSymbols
+
+struct UsingSFSymbolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
