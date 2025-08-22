6bedaeddbad54d8e66578e36ef7564a51a879161
Marco Bassaletti <bassaletti@gmail.com>
2025-08-22 10:47:39 -0400

    [Book23-SwiftUI][Ch2] List View Edit Row

---
+++ book23-swiftui/16-ListRowEdit/ListRowEdit/ListRowEditApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListRowEditApp.swift
+//  ListRowEdit
+//
+//  Created by Marco Bassaletti on 22-08-25.
+//
+
+import SwiftUI
+
+@main
+struct ListRowEditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
