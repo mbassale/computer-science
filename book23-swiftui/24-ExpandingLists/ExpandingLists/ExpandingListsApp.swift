9da02c928ed0c1649bae5894d3d2239d81ae0881
Marco Bassaletti <bassaletti@gmail.com>
2025-08-24 12:37:24 -0400

    [Book23-SwiftUI][Ch3] Expanding Lists (Hierarchical Content)

---
+++ book23-swiftui/24-ExpandingLists/ExpandingLists/ExpandingListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandingListsApp.swift
+//  ExpandingLists
+//
+//  Created by Marco Bassaletti on 24-08-25.
+//
+
+import SwiftUI
+
+@main
+struct ExpandingListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ book23-swiftui/24-ExpandingLists/ExpandingListsTests/ExpandingListsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandingListsTests.swift
+//  ExpandingListsTests
+//
+//  Created by Marco Bassaletti on 24-08-25.
+//
+
+import Testing
+@testable import ExpandingLists
+
+struct ExpandingListsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
